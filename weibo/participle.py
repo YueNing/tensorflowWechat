76b14f29a4068c27894e6e471c2bcd1f815d17c2
YueNing <n1085633848@outlook.com>
2018-01-19 23:46:59 +0100

    添加中文分词器jieba并实现和word_cloud的结合, 通过participle 文件下的WordCloud_CN类(继承WordCloud),同时添加静态文件停用词和中文字体文件ttf

---
+++ weibo/participle.py
@@ -0,0 +1,49 @@
+from os import path
+import numpy as np
+import matplotlib.pyplot as plt
+from wordcloud import WordCloud, STOPWORDS
+import jieba
+
+class WordCloud_CN(WordCloud):
+	"""docstring for WordCloud_CN"""
+	def __init__(self, stopwords_file=None, text_file=None, font_path=None):
+		super(WordCloud_CN, self).__init__(font_path=font_path, background_color='black', margin=5, width=1800, height=800)
+		if stopwords_file == None:
+			stopwords_file = u'statics/stopwords.txt'
+		if text_file == None:
+			text_file = u'weibo_image/1683227315_words/context.txt'
+		self.stopwords_file = stopwords_file
+		self.text_file = text_file
+
+	@property
+	def get_stopwords(self):
+		self.stopwords = {}
+		with open(self.stopwords_file, 'r') as swf:
+			line = swf.readline().rstrip()
+			while line:
+				self.stopwords.setdefault(line, 0)
+				self.stopwords[line.decode('utf-8')] = 1
+				line = swf.readline().rstrip()
+		return self.stopwords
+
+	@property
+	def seg_text(self):
+		with open(self.text_file) as textf:
+			text = textf.readlines()
+			text = r' '.join(text)
+
+			seg_generator = jieba.cut(text)
+			self.seg_list = [
+				i for i in seg_generator if i not in self.get_stopwords
+			]
+			self.seg_list = [i for i in self.seg_list if i !=u' ']
+			self.seg_list = r' '.join(self.seg_list)
+		return self.seg_list
+
+	def show(self):
+		wordcloud = self.generate(self.seg_text)
+
+		plt.figure()
+		plt.imshow(wordcloud)
+		plt.axis("off")
+		plt.show()
--- weibo/word_cloud.py
+++ weibo/word_cloud.py
@@ -1,25 +1,14 @@
 # -*- coding: utf-8 -*-
-
 from os import path
-from wordcloud import WordCloud
-
+from participle import WordCloud_CN
 # __file__返回脚本完整路径 包括路径和文件名
 d = path.dirname(__file__)
 
-# 读取整个文本
-text = open(path.join(d, 'weibo_image/1683227315_words/context.txt')).read()
-wordcloud = WordCloud().generate(text)
 
-# 显示生成的图片
-# 通过matplotlib 实现
-import matplotlib.pyplot as plt 
-# plt.imshow(wordcloud, interpolation='bilinear')
-# plt.axis("off")
+STOPWORDS_FILE = path.join(d, u'statics/stopwords.txt')
+TEXT_FILE = path.join(d, u'1.weibo_image/2687827715_words/context.txt')
+FONT_PATH = path.join(d, u'./statics/simheittf/simhei.ttf')
 
-# 小字体
-wordcloud = WordCloud(max_font_size=40).generate(text)
-plt.figure()
-plt.imshow(wordcloud, interpolation='bilinear')
-plt.axis("off")
-
-plt.show()
+# 生成词云
+wordcloud = WordCloud_CN(stopwords_file=STOPWORDS_FILE, text_file=TEXT_FILE, font_path=FONT_PATH)
+wordcloud.show()
