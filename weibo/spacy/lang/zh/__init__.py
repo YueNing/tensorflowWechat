776c50f219ecf535b2988042cdabd72d53118da2
YueNing <n1085633848@outlook.com>
2018-01-21 02:45:04 +0100

    complete using spacy

--- weibo/spacy/lang/zh/__init__.py
+++ weibo/spacy/lang/zh/__init__.py
@@ -4,16 +4,56 @@ from __future__ import unicode_literals
 from ...attrs import LANG
 from ...language import Language
 from ...tokens import Doc
-
+import jieba
 
 class ChineseDefaults(Language.Defaults):
     lex_attr_getters = dict(Language.Defaults.lex_attr_getters)
     lex_attr_getters[LANG] = lambda text: 'zh'  # for pickling
 
 
+class StopWords(object):
+    """docstring for StopWords"""
+    def __init__(self, stopwords_file=None, text_file=None, font_path=None):
+        super(StopWords, self).__init__()
+        self.stopwords_file = stopwords_file
+        self.text_file = text_file
+
+    @property
+    def get_text_file(self):
+        return self.text_file
+
+    @property
+    def get_stopwords(self):
+        self.stopwords = {}
+        with open(self.stopwords_file, 'r') as swf:
+            line = swf.readline().rstrip()
+            while line:
+                self.stopwords.setdefault(line, 0)
+                self.stopwords[line] = 1
+                line = swf.readline().rstrip()
+        return self.stopwords
+
+    @property
+    def seg_text(self):
+        with open(self.text_file) as textf:
+            text = textf.readlines()
+            text = r' '.join(text)
+
+            seg_generator = jieba.cut(text)
+            self.seg_list = [
+                i for i in seg_generator if i not in self.get_stopwords
+            ]
+            self.seg_list = [i for i in self.seg_list if i !=u' ']
+            self.seg_list = r' '.join(self.seg_list)
+        return self.seg_list
+
 class Chinese(Language):
     lang = 'zh'
     Defaults = ChineseDefaults  # override defaults
+
+    def __init__(self, stopwords_file=None, text_file=None):
+        Language.__init__(self)
+        self.stopwords = StopWords(stopwords_file = stopwords_file, text_file = text_file)
 
     def make_doc(self, text):
         try:
@@ -21,7 +61,13 @@ class Chinese(Language):
         except ImportError:
             raise ImportError("The Chinese tokenizer requires the Jieba library: "
                               "https://github.com/fxsjy/jieba")
-        words = list(jieba.cut(text, cut_all=False))
+    
+        # add code to remove custome stopwords
+        if self.stopwords.get_text_file:
+            words = self.stopwords.seg_text
+        if text:
+            words = list(jieba.cut(text, cut_all=False))
+
         words = [x for x in words if x]
         return Doc(self.vocab, words=words, spaces=[False]*len(words))
 
--- weibo/word_cloud.py
+++ weibo/word_cloud.py
@@ -1,14 +1,25 @@
 # -*- coding: utf-8 -*-
+from spacy.lang.zh import Chinese
 from os import path
-from participle import WordCloud_CN
+import matplotlib.pyplot as plt
+from wordcloud import WordCloud, STOPWORDS
+
 # __file__返回脚本完整路径 包括路径和文件名
 d = path.dirname(__file__)
 
 
 STOPWORDS_FILE = path.join(d, u'statics/stopwords.txt')
-TEXT_FILE = path.join(d, u'1.weibo_image/2687827715_words/context.txt')
+TEXT_FILE = path.join(d, u'1.weibo_image/1683227315_words/context.txt')
 FONT_PATH = path.join(d, u'./statics/simheittf/simhei.ttf')
 
+def show(seg_text = None):
+	wordcloud = WordCloud(font_path=FONT_PATH, background_color='black', margin=5, width=1800, height=800).generate(str(seg_text))
+	plt.figure()
+	plt.imshow(wordcloud)
+	plt.axis("off")
+	plt.show()
+
 # 生成词云
-wordcloud = WordCloud_CN(stopwords_file=STOPWORDS_FILE, text_file=TEXT_FILE, font_path=FONT_PATH)
-wordcloud.show()
+nlp = Chinese(text_file = TEXT_FILE, stopwords_file = STOPWORDS_FILE)
+doc = nlp(None)
+show(seg_text=doc)
