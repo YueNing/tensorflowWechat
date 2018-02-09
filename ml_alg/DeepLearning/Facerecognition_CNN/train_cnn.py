8e5aa14d978eb6b563cf8a76e23e2f900de4c21b
Yue <n1085633848@outlook.com>
2018-02-09 14:39:21 +0100

    LeNet Facerecognition CNN

---
+++ ml_alg/DeepLearning/Facerecognition_CNN/train_cnn.py
@@ -0,0 +1,41 @@
+import os
+import sys
+import time
+
+import numpy as np
+from PIL import Image
+
+import theano
+import theano.tensor as T
+from theano.tensor.signal import downsample
+from theano.tensor.nnet import conv
+
+def load_data(dataset_path):
+    return
+
+def shared_dataset(data_x, data_y, borrow=True):
+    return
+
+class LogisticRegression(object):
+
+    def __init__(self, input, n_in, n_out):
+
+    def negative_log_likelihood(self, y):
+        return
+    def errors(self, y):
+        return
+
+class HiddenLayer(obejct):
+    def __init__():
+
+class LeNetConvPoolLayer(object):
+    
+    def __init__():
+
+def save_params(param1, param2, param3, param4):
+
+def evaluate_olivettifaces():
+    return
+
+if __name__ == "__main__":
+    evaluate_olivettifaces()
