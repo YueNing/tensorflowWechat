5099dccbe464fc1a751ca0cc214348d3fef541cc
Yue <n1085633848@outlook.com>
2018-02-10 15:28:59 +0100

    add RNN_TF function

---
+++ ml_alg/DeepLearning/RNN_TF/rnn.py
@@ -0,0 +1,38 @@
+import tensorflow as tf
+import numpy as np 
+import matplotlib.pyplot as plt
+
+
+def get_batch():
+    return
+
+class LSTMRNN(obejct):
+    def __init__(self, n_steps, input_size, output_size, cell_size, batch_size):
+        self.n_steps = n_steps
+        self.input_size = input_size
+        self.output_size = output_size
+
+    def add_input_layer(self, ):
+        return
+
+    def add_cell(self):
+        return 
+    
+    def add_output_layer(self):
+        return
+
+    def compute_cost(self):
+        return
+
+    @staticmethod
+    def ms_errror(labels, logits):
+        return
+
+    def _weight_variable(self, shape, name='weights'):
+        return
+
+    def _bias_variable(self, shape, name='biases'):
+        return
+
+
+if __name__ == '__main__':
