41e12c623417c666734f7dbf28992595a7334bf8
Yue <n1085633848@outlook.com>
2018-02-10 15:42:47 +0100

    add code of function get_batch

--- ml_alg/DeepLearning/RNN_TF/rnn.py
+++ ml_alg/DeepLearning/RNN_TF/rnn.py
@@ -2,14 +2,26 @@ import tensorflow as tf
 import numpy as np 
 import matplotlib.pyplot as plt
 
-
+# Net Parameter
+BATCH_START = 0
+TIME_STEPS  = 20
+BATCH_SIZE  = 50
+INPUT_SIZE  = 1
+OUPUT_SIZE  = 1
+CELL_SIZE   = 10
+LR          = 0.006
 def get_batch():
-    return
+    global BATCH_START, TIME_STEPS
+    xs = np.arange() / (10 * np.pi)
+    seq = np.sin(xs)
+    res = np.cos(xs)
+    BATCH_START += TIME_STEPS
+    return [seq[:, :, np.newaxis], res[:, :, np.newaxis], xs]
 
 class LSTMRNN(obejct):
     def __init__(self, n_steps, input_size, output_size, cell_size, batch_size):
-        self.n_steps = n_steps
-        self.input_size = input_size
+        self.n_steps     = n_steps
+        self.input_size  = input_size
         self.output_size = output_size
 
     def add_input_layer(self, ):
