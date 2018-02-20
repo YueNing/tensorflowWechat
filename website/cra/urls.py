815ec1045122a28d830b26af7b774152fe5f73b7
Yue <n1085633848@outlook.com>
2018-02-20 23:28:59 +0100

    add applehome

--- website/cra/urls.py
+++ website/cra/urls.py
@@ -3,6 +3,7 @@ from . import views
 
 urlpatterns = [
 	path('', views.index, name='index'),
+	path('AppleHome', views.AppleHome, name='AppleHome'),
 	path('Vorlesungen/ML', views.ML, name='vorlesung_ML'),
 	path('Gallery/Hi', views.Gallery, name='Hi'),
 	path('Data/Weibo', views.DataWeibo, name='Data_weibo'),
--- website/cra/views.py
+++ website/cra/views.py
@@ -1,7 +1,7 @@
 import sys, os
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 sys.path.append('/home/nauen/env/website/tensorflowWechat')
-import weibo
+#import weibo
 from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
@@ -9,7 +9,9 @@ from django.http import HttpResponse
 # def index(request):
 # 	return HttpResponse('Hello, Intelligent')
 
-
+def AppleHome(request):
+	return render(request, 'cra/applehome.html')
+	
 def index(request):
 	context = {}
 	context['hello'] = 'Hello Word!'
